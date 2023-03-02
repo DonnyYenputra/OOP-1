1d7a90b46003c2884abb2d683781b04bb5dde1f7
阮瑋傑(Donny Yenputra) <61980656+DonnyYenputra@users.noreply.github.com>
2023-03-02 23:58:46 +0800

    Add files via upload

---
+++ A1083328_0224_1.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class A1083328_0224_1{
+    public static void main(String [] args){
+        Scanner sc = new Scanner(System.in);
+        int angka;
+        System.out.println("Checking The ODD or EVEN Number");
+        System.out.println("-------------------------------");
+        System.out.print("Input the number = ");
+        angka = sc.nextInt();
+
+        if(angka%2==0){
+            System.out.println("Number "+angka+" is Even");
+        }else{
+            System.out.println("Number "+angka+" is Odd");
+        }
+    }
+}
---
+++ A1083328_0224_2.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class A1083328_0224_2{
+    public static void main(String [] args){
+        Scanner sc = new Scanner(System.in);
+        double C, F;
+        System.out.println("The changing temperature from Celsius to Fahrenheit");
+        System.out.println("---------------------------------------------------");
+        System.out.print("Input the degrees = ");
+        C = sc.nextDouble();
+
+        F = (C * 9/5) + 32;
+        System.out.print("The Result of "+C+" degrees Celsius into Fahrenheit is "+F+" degrees");
+    }
+}

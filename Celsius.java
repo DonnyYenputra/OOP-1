1de19b54fe9b35ec5d38536e7550925edff4c1fc
阮瑋傑(Donny Yenputra) <61980656+DonnyYenputra@users.noreply.github.com>
2023-03-02 23:51:15 +0800

    Add files via upload

---
+++ Celsius.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Celsius{
+    public static void main(String [] args){
+        Scanner sc = new Scanner(System.in);
+        double C, F;
+        System.out.println("The changing temperature from Celsius to Fahrenheit");
+        System.out.println("---------------------------------------------------");
+        System.out.print("Input the degrees = ");
+        C = sc.nextDouble();
+
+        F = (C * 9/5) + 32;
+        System.out.print("The Result of "+C+" degress Celsius into Fahrenheit is "+F);
+    }
+}
